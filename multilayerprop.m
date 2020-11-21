de2434d0b6c6462f7e617d0514fe314f7e1540ea
zjlin <zijunlin32@gmail.com>
2020-11-20 14:08:25 -1000

    Create multilayerprop.m

---
+++ multilayerprop.m
@@ -0,0 +1,10 @@
+%get user inputs 
+n = input("Enter the number of layers")
+%get parameters of each region 
+%conductivity 
+c =
+%permitivity 
+e =
+%permeability
+u =
+%for loop to get the input data
