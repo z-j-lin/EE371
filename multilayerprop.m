32feae0b2dd5aa5671457317d3d96c875e75d501
zjlin <zijunlin32@gmail.com>
2020-12-01 13:48:48 -1000

    added gamas

--- multilayerprop.m
+++ multilayerprop.m
@@ -1,5 +1,7 @@
 %get user inputs 
 N = input("Enter the number of layers");
+f = input("Enter the number of layers");
+w = 2*pi/f;
 %get parameters of each region 
 %permitivity of free space
 e0= 8.85418782e-12;
@@ -26,7 +28,9 @@ for v = 1:N
     d(v) = input("Enter the thickness of region" + v);
     %calculate characteristic impedance for each region
     n(v) = n0/sqrt(e(v));
-    propconst(v)= 
+    alpha = w*sqrt(u(v)*e(v)/2)*sqrt(sqrt(1+((c(v)/w*e(v))^2)-1));
+    beta = w*sqrt(u(v)*e(v)/2)*sqrt(sqrt(1+((c(v)/w*e(v))^2)+1));
+    propconst(v)= alpha + ibeta;
 end
 %instantiate arrays to store values for impedance and reflection coefficient
 z = zeros(N);
@@ -35,9 +39,14 @@ rp = zeros(N-1);
 r = zeros(N);
 z(N)= n(N);
 z(n-1) = z(n);
+%calculate impedences 
 for v = N-1:-1:1
     r(v) = (z(v)- n(v))/(z(v)+n(v));
-    rp(v) = r(v)*e^(2*(g*-d(v)));
+    rp(v) = r(v)*exp(2*propconst(v)*-d(v));
     zp(v) = n(v)*((1+rp(v))/(1-rp(v)));
     z(v-1) = zp(v);
 end
+%calculate the outputs 
+for v = 0 :1:N
+    
+end 
